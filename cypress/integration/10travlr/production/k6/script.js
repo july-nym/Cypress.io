69c56e9d161daeef61a6c3be0f39802aa78020c6
july-nym <nym.july@gmail.com>
2020-12-30 19:26:16 +0800

    #GO

--- cypress/integration/10travlr/production/k6/script.js
+++ cypress/integration/10travlr/production/k6/script.js
@@ -1,9 +1,37 @@
-import { check } from 'k6';
 import http from 'k6/http';
+import { check, group, sleep, fail } from 'k6';
 
-export default function () {
-  let res = http.get('http://test.k6.io/');
-  check(res, {
-    'is status 200': (r) => r.status === 200,
+export let options = {
+  vus: 1, // 1 user looping for 1 minute
+  duration: '1m',
+
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+  },
+};
+
+const BASE_URL = 'https://test-api.k6.io';
+const USERNAME = 'TestUser';
+const PASSWORD = 'SuperCroc2020';
+
+export default () => {
+  let loginRes = http.post(`${BASE_URL}/auth/token/login/`, {
+    username: USERNAME,
+    password: PASSWORD,
   });
-}
+
+  check(loginRes, {
+    'logged in successfully': (resp) => resp.json('access') !== '',
+  });
+
+  let authHeaders = {
+    headers: {
+      Authorization: `Bearer ${loginRes.json('access')}`,
+    },
+  };
+
+  let myObjects = http.get(`${BASE_URL}/my/crocodiles/`, authHeaders).json();
+  check(myObjects, { 'retrieved crocodiles': (obj) => obj.length > 0 });
+
+  sleep(1);
+};
