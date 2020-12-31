53ddd7974c01ad762246d938258d55efccde97d4
july-nym <nym.july@gmail.com>
2020-12-31 11:06:20 +0800

    test

--- cypress/integration/10travlr/production/k6/script.js
+++ cypress/integration/10travlr/production/k6/script.js
@@ -1,12 +1,15 @@
 import http from 'k6/http';
-import { check, group, sleep, fail } from 'k6';
+import { check, group, sleep } from 'k6';
 
 export let options = {
-  vus: 1, // 1 user looping for 1 minute
-  duration: '1m',
-
+  stages: [
+    { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+    { duration: '10m', target: 100 }, // stay at 100 users for 10 minutes
+    { duration: '5m', target: 0 }, // ramp-down to 0 users
+  ],
   thresholds: {
     http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
+    'logged in successfully': ['p(99)<1500'], // 99% of requests must complete below 1.5s
   },
 };
 
