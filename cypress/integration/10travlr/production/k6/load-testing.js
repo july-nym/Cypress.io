6faf664cba0f83703de228367b8ffd1a8bca9470
july-nym <nym.july@gmail.com>
2020-12-31 19:12:54 +0800

    test

--- cypress/integration/10travlr/production/k6/load-testing.js
+++ cypress/integration/10travlr/production/k6/load-testing.js
@@ -3,8 +3,8 @@ import { check, group, sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '5m', target: 100 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
-    { duration: '10m', target: 100 }, // stay at 100 users for 10 minutes
+    { duration: '5m', target: 20 }, // simulate ramp-up of traffic from 1 to 100 users over 5 minutes.
+    { duration: '10m', target: 20 }, // stay at 100 users for 10 minutes
     { duration: '5m', target: 0 }, // ramp-down to 0 users
   ],
   thresholds: {
