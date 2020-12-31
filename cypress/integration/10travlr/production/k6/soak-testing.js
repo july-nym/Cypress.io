a286c9d6141de28a3d44954ce9fe6df374906849
july-nym <nym.july@gmail.com>
2020-12-31 19:53:42 +0800

    #k6-improvement

--- cypress/integration/10travlr/production/k6/soak-testing.js
+++ cypress/integration/10travlr/production/k6/soak-testing.js
@@ -3,8 +3,8 @@ import { sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '2m', target: 400 }, // ramp up to 400 users
-    { duration: '3h56m', target: 400 }, // stay at 400 for ~4 hours
+    { duration: '2m', target: 25 }, // ramp up to 400 users
+    { duration: '5m', target: 25 }, // stay at 400 for ~4 hours
     { duration: '2m', target: 0 }, // scale down. (optional)
   ],
 };
@@ -14,7 +14,7 @@ const API_BASE_URL = 'https://test-api.k6.io';
 export default function () {
   http.batch([
     ['GET', `${API_BASE_URL}/public/crocodiles/1/`],
-    ['GET', `${API_BASE_URL}/public/crocodiles/2/`],
+    ['GET', `${API_BASE_URL}/public/crocodååiles/2/`],
     ['GET', `${API_BASE_URL}/public/crocodiles/3/`],
     ['GET', `${API_BASE_URL}/public/crocodiles/4/`],
   ]);
--- cypress/integration/10travlr/production/k6/stress-testing.js
+++ cypress/integration/10travlr/production/k6/stress-testing.js
@@ -3,15 +3,15 @@ import { sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '2m', target: 100 }, // below normal load
-    { duration: '5m', target: 100 },
-    { duration: '2m', target: 200 }, // normal load
-    { duration: '5m', target: 200 },
-    { duration: '2m', target: 300 }, // around the breaking point
-    { duration: '5m', target: 300 },
-    { duration: '2m', target: 400 }, // beyond the breaking point
-    { duration: '5m', target: 400 },
-    { duration: '10m', target: 0 }, // scale down. Recovery stage.
+    { duration: '1m', target: 5 }, // below normal load
+    { duration: '1m', target: 5 },
+    { duration: '1m', target: 5 }, // normal load
+    { duration: '1m', target: 5 },
+    { duration: '1m', target: 5 }, // around the breaking point
+    { duration: '1m', target: 5 },
+    { duration: '1m', target: 5 }, // beyond the breaking point
+    { duration: '1m', target: 5 },
+    { duration: '1m', target: 0 }, // scale down. Recovery stage.
   ],
 };
 
